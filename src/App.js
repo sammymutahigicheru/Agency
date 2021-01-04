01d8e26d02665a81eb59d40c4f6d3cf804712103
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-04 10:55:13 +0300

    app component to include home component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import PageWrapper from './components/PageWrapper';
-
+import Home from './components/pages/Home';
 function App() {
   return (
-    <PageWrapper />
+    <PageWrapper>
+      <Home />
+    </PageWrapper>
   );
 }
 
