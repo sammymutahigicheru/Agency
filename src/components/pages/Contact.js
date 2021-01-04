4a05550066d724d8fdc27d1df9b8111763bd961e
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-04 20:21:59 +0300

    dynamically set the forms

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,19 @@
 import React,{Component} from 'react';
+import Field from '../common/Field';
+
+const fields = {
+    sections: [
+       [
+        {name:'name',elementName: 'input', type: 'text',placeholder:'Your Name'},
+        {name:'email',elementName: 'input', type: 'email',placeholder:'Your Email'},
+        {name:'phone',elementName: 'input', type: 'text',placeholder:'Your Phone'}
+       ],
+        [
+            {name:'message',elementName: 'textarea', type: 'text',placeholder:'Type Your Message'}
+        ]
+    ],
+    
+}
 
 class Contact extends Component{
     constructor(props){
@@ -20,56 +35,22 @@ class Contact extends Component{
                 </div>
                 <form id="contactForm" name="sentMessage" novalidate="novalidate">
                     <div className="row align-items-stretch mb-5">
-                        <div className="col-md-6">
-                            <div className="form-group">
-                                <input className="form-control"
-                                 id="name" 
-                                 type="text" 
-                                 placeholder="Your Name *" 
-                                 required="required" 
-                                 data-validation-required-message="Please enter your name." 
-                                 value = {this.state.name}
-                                 onChange={e => this.setState({name: e.target.value})}
-                                 />
-                                <p className="help-block text-danger"></p>
-                            </div>
-                            <div className="form-group">
-                                <input className="form-control"
-                                 id="email" 
-                                 type="email"
-                                  placeholder="Your Email *" 
-                                  required="required" 
-                                  data-validation-required-message="Please enter your email address." 
-                                  value = {this.state.email}
-                                  onChange = {e => this.setState({email: e.target.value})}
-                                  />
-                                <p className="help-block text-danger"></p>
-                            </div>
-                            <div className="form-group mb-md-0">
-                                <input className="form-control" 
-                                id="phone" type="tel" 
-                                placeholder="Your Phone *" 
-                                required="required"
-                                 data-validation-required-message="Please enter your phone number." 
-                                 value = {this.state.phone}
-                                 onChange = {e => this.setState({phone: e.target.value})}
-                                 />
-                                <p className="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="form-group form-group-textarea mb-md-0">
-                                <textarea className="form-control" 
-                                id="message" 
-                                placeholder="Your Message *" 
-                                required="required" 
-                                data-validation-required-message="Please enter a message." 
-                                value = {this.state.message}
-                                onChange = {e => this.setState({message: e.target.value})}
-                                />
-                                <p className="help-block text-danger"></p>
-                            </div>
-                        </div>
+                           {fields.sections.map((section,sectionIndex) =>{
+                               return (
+                                <div className="col-md-6" key={sectionIndex}>
+                                    {section.map((field,i)=>{
+                                        return <Field 
+                                        {...field}
+                                        key={i}
+                                        value = {this.state[field.name]}
+                                        onChange = {e => this.setState({
+                                            [field.name]: e.target.value
+                                        })}
+                                        />
+                                    })}
+                                </div>
+                               );
+                           })} 
                     </div>
                     <div className="text-center">
                         <div id="success"></div>
