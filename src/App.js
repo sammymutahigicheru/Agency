a2aa51623fae02a007e5a43c5959dc351da65bb0
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-04 19:27:58 +0300

    add event handler

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import PageWrapper from './components/PageWrapper';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import { BrowserRouter as Router, Route,Link } from "react-router-dom";
+import Contact from './components/pages/Contact';
 function App() {
   return (
     <Router>
@@ -17,6 +18,11 @@ function App() {
           path="/about"
           component={About}
        />
+
+       <Route 
+       path = "/contact"
+       component = {Contact}
+      />
   
       </PageWrapper>
     </Router>
