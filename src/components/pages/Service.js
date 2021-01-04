784f521a9356f93168c71aadf67919d961664093
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-04 14:46:24 +0300

    create component Portfolio

--- src/components/pages/Service.js
+++ src/components/pages/Service.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react';
 import SingleService from '../common/SingleService';
 
 const services = [
-    {title:'E-Commerce',description: '',icon: 'fa-shopping-cart'}
+    {title:'E-Commerce',description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.',icon: 'fa-shopping-cart'},
+    {title:'Responsive Design',description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.',icon: 'fa-laptop'},
+    {title:'Web Security',description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.',icon: 'fa-lock'}
 ];
 
 class Service extends Component{
