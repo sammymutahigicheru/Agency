fd00571602124944480b00864454a576022e8c20
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-04 11:48:56 +0300

    create Header dynamic component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import PageWrapper from './components/PageWrapper';
 import Home from './components/pages/Home';
+import About from './components/pages/About';
 import { BrowserRouter as Router, Route,Link } from "react-router-dom";
 function App() {
   return (
     <Router>
       <PageWrapper>
-        
+
         <Route
+          exact = {true}
           path="/"
           component={Home}
         />
+
+        <Route
+          path="/about"
+          component={About}
+       />
   
       </PageWrapper>
     </Router>
