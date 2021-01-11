4c830fbdd8384f7748bccbb6c5471410fa49acbe
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-11 14:39:42 +0300

    add form validation with Yup and formik

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
+import {withFormik} from 'formik';
+import Field from '../common/Field';
+import * as Yup from 'yup';
 
+const field = [
+    {name: 'email',elementName: 'input',type:'email',placeholder: 'Your Email'},
+    {name: 'password',elementName: 'input',type: 'password',placeholder: 'Your Password'}
+]
 
 class Login extends Component{
     render(){
@@ -8,4 +15,16 @@ class Login extends Component{
         )
     }
 }
-export default Login;
+export default  withFormik({
+    mapPropsToValues: () =>({
+        email: '',
+        password: ''
+    }),
+    validationSchema: Yup.object().shape({
+        email: Yup.string().email('Email is invalid').required('You need to log in with email address'),
+        password: Yup.string().required('You need to enter your password')
+    }),
+    handleSubmit: (values, {setSubmitting}) =>{
+        console.log('Login attemp',values);
+    }
+})(Login);
