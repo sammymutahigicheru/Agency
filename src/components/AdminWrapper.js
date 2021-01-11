ee5bab0f54c6807ddc5fcb66f94cb6fcc6b61eef
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-11 13:05:07 +0300

    add admin wrapper

---
+++ src/components/AdminWrapper.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class AdminWrapper extends Component{
+    render(){
+        return(
+            <div id="admin-page">
+                {this.props.children}
+            </div>
+        )
+    }
+}
+export default AdminWrapper;
